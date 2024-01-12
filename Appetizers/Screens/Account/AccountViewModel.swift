ac8f3522e15dd42f5a19b2d5c7b9ebf96d2d1be0
Azizbek Rasulov <khan.ak33.ak47@gmail.com>
2024-01-13 03:23:57 +0500

    Refactored guards and regex

--- Appetizers/Screens/Account/AccountViewModel.swift
+++ Appetizers/Screens/Account/AccountViewModel.swift
@@ -29,7 +29,7 @@ final class AccountViewModel: ObservableObject {
     }
     
     func getUser() {
-        guard let userData = userData else { return }
+        guard let userData else { return }
         
         do {
             user = try JSONDecoder().decode(User.self, from: userData)
